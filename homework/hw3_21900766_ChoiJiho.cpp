e60ffa3da3e4770b5bc688e477064dab0a345209
Zho <chlwlgh1011@naver.com>
2022-03-18 16:24:30 +0900

    yeah

--- homework/hw3_21900766_ChoiJiho.cpp
+++ homework/hw3_21900766_ChoiJiho.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 using namespace std;
 
+class mystack
+{
+    int stack[100];
+    int top;
+    public:
+        void push(int a);
+        int pop();
+        bool stack_empty();
+        void init();
+};
+
+
+void mystack::init()
+{
+    int top = 0;
+}
+
+void mystack::push(int a)
+{
+    stack[top] = a;
+    top++;
+}
+
+int mystack::pop()
+{
+    top--;
+    return(stack[top]);
+}
+
+bool mystack::stack_empty()
+{
+    if(top == 0) return true;
+    else return false;
+}
+
 int main()
 {
-    mystack  s1;
+    mystack s1;
     int list[5] = { 32, 123, 27, 131, 242 }, i, x;
     s1.init();
     for (i = 0; i < 5; i++)
@@ -14,6 +49,6 @@ int main()
         x = s1.pop( );
         cout << x << endl ; 
     }
-    
     return 0;
 }
+
