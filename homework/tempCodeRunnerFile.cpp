c20b41411e5e812070eab5ff69ee7f939716aa4c
Zho <chlwlgh1011@naver.com>
2022-05-17 22:39:55 +0900

    hw13solve

--- homework/tempCodeRunnerFile.cpp
+++ homework/tempCodeRunnerFile.cpp
@@ -1,1 +1,6 @@
-bst_tree();
+   // case SHOWPRE: t1.show_preorder();
+            //     break;
+            // case SHOWPOST: t1.show_postorder();
+            //     break;
+            // case SEARCH: temp = t1.search(birth_day_key);
+            //     break;
